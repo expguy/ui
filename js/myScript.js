5c0d2310556a97486d4aabd902e630d08d93f342
expguy <t2110gh.bxduc@aptech.vn>
2022-09-20 00:38:50 +0700

    UI

---
+++ js/myScript.js
@@ -0,0 +1,53 @@
+function show1(){
+    document.getElementById("box-arrow-top-1").style.display = "block";
+    document.getElementById("box-arrow-top-2").style.display = "none";
+    document.getElementById("box-arrow-top-3").style.display = "none";
+    document.getElementById("box-arrow-top-4").style.display = "none";
+    document.getElementById("box-arrow-top-5").style.display = "none";
+    document.getElementById("box-arrow-top-6").style.display = "none";
+}
+
+function show2(){
+    document.getElementById("box-arrow-top-2").style.display = "block";
+    document.getElementById("box-arrow-top-1").style.display = "none";
+    document.getElementById("box-arrow-top-3").style.display = "none";
+    document.getElementById("box-arrow-top-4").style.display = "none";
+    document.getElementById("box-arrow-top-5").style.display = "none";
+    document.getElementById("box-arrow-top-6").style.display = "none";
+}
+
+function show3(){
+    document.getElementById("box-arrow-top-3").style.display = "block";
+    document.getElementById("box-arrow-top-1").style.display = "none";
+    document.getElementById("box-arrow-top-2").style.display = "none";
+    document.getElementById("box-arrow-top-4").style.display = "none";
+    document.getElementById("box-arrow-top-5").style.display = "none";
+    document.getElementById("box-arrow-top-6").style.display = "none";
+}
+
+function show4(){
+    document.getElementById("box-arrow-top-4").style.display = "block";
+    document.getElementById("box-arrow-top-1").style.display = "none";
+    document.getElementById("box-arrow-top-2").style.display = "none";
+    document.getElementById("box-arrow-top-3").style.display = "none";
+    document.getElementById("box-arrow-top-5").style.display = "none";
+    document.getElementById("box-arrow-top-6").style.display = "none";
+}
+
+function show5(){
+    document.getElementById("box-arrow-top-5").style.display = "block";
+    document.getElementById("box-arrow-top-1").style.display = "none";
+    document.getElementById("box-arrow-top-2").style.display = "none";
+    document.getElementById("box-arrow-top-3").style.display = "none";
+    document.getElementById("box-arrow-top-4").style.display = "none";
+    document.getElementById("box-arrow-top-6").style.display = "none";
+}
+
+function show6(){
+    document.getElementById("box-arrow-top-6").style.display = "block";
+    document.getElementById("box-arrow-top-1").style.display = "none";
+    document.getElementById("box-arrow-top-2").style.display = "none";
+    document.getElementById("box-arrow-top-3").style.display = "none";
+    document.getElementById("box-arrow-top-4").style.display = "none";
+    document.getElementById("box-arrow-top-5").style.display = "none";
+}
