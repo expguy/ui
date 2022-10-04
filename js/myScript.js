17663d10359012fad4c26f589c3224c2250fd4b4
expguy <t2110gh.bxduc@aptech.vn>
2022-10-04 21:21:07 +0700

    Cart site

--- js/myScript.js
+++ js/myScript.js
@@ -1,4 +1,4 @@
-function show1(){
+function show1() {
     document.getElementById("box-arrow-top-1").style.display = "block";
     document.getElementById("box-arrow-top-2").style.display = "none";
     document.getElementById("box-arrow-top-3").style.display = "none";
@@ -7,7 +7,7 @@ function show1(){
     document.getElementById("box-arrow-top-6").style.display = "none";
 }
 
-function show2(){
+function show2() {
     document.getElementById("box-arrow-top-2").style.display = "block";
     document.getElementById("box-arrow-top-1").style.display = "none";
     document.getElementById("box-arrow-top-3").style.display = "none";
@@ -16,7 +16,7 @@ function show2(){
     document.getElementById("box-arrow-top-6").style.display = "none";
 }
 
-function show3(){
+function show3() {
     document.getElementById("box-arrow-top-3").style.display = "block";
     document.getElementById("box-arrow-top-1").style.display = "none";
     document.getElementById("box-arrow-top-2").style.display = "none";
@@ -25,7 +25,7 @@ function show3(){
     document.getElementById("box-arrow-top-6").style.display = "none";
 }
 
-function show4(){
+function show4() {
     document.getElementById("box-arrow-top-4").style.display = "block";
     document.getElementById("box-arrow-top-1").style.display = "none";
     document.getElementById("box-arrow-top-2").style.display = "none";
@@ -34,7 +34,7 @@ function show4(){
     document.getElementById("box-arrow-top-6").style.display = "none";
 }
 
-function show5(){
+function show5() {
     document.getElementById("box-arrow-top-5").style.display = "block";
     document.getElementById("box-arrow-top-1").style.display = "none";
     document.getElementById("box-arrow-top-2").style.display = "none";
@@ -43,7 +43,7 @@ function show5(){
     document.getElementById("box-arrow-top-6").style.display = "none";
 }
 
-function show6(){
+function show6() {
     document.getElementById("box-arrow-top-6").style.display = "block";
     document.getElementById("box-arrow-top-1").style.display = "none";
     document.getElementById("box-arrow-top-2").style.display = "none";
@@ -51,3 +51,38 @@ function show6(){
     document.getElementById("box-arrow-top-4").style.display = "none";
     document.getElementById("box-arrow-top-5").style.display = "none";
 }
+
+$(document).ready(function () {
+
+    var quantitiy = 0;
+    $('.quantity-right-plus').click(function (e) {
+
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+        var quantity = parseInt($('#quantity').val());
+
+        // If is not undefined
+
+        $('#quantity').val(quantity + 1);
+
+
+        // Increment
+
+    });
+
+    $('.quantity-left-minus').click(function (e) {
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+        var quantity = parseInt($('#quantity').val());
+
+        // If is not undefined
+
+        // Increment
+        if (quantity > 1) {
+            $('#quantity').val(quantity - 1);
+        }
+    });
+
+});
